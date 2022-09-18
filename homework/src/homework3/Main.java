5a5259195e30144943c5e50fa37e7f04feb29277
Nikolay1992167 <wulik1081@mail.ru>
2022-09-18 22:10:42 +0300

    fixed

--- homework/src/homework3/Main.java
+++ homework/src/homework3/Main.java
@@ -2,26 +2,26 @@ package homework3;
 
 public class Main {
     public static void main(String[] args) {
-        mainArray(-100000, 100000);
-        divisionIntoPositiveAndNegativeNumbers(mainArray(-100000, 100000));
-        arrayPrimeNumbers(mainArray(-100000, 100000));
+        divisionIntoPositiveAndNegativeNumbers(mainArray());
+        arrayPrimeNumbers(mainArray());
     }
-    private static int[] mainArray(int beginningOfTheInterval, int endOfTheInterval) {
-        int arraySize = 0;
-        for (int i = beginningOfTheInterval; i <= endOfTheInterval; i++) {
-            arraySize = arraySize + 1;
-        }
+
+    private static int[] mainArray() {
+        int beginningOfTheInterval = -100000;
+        int endOfTheInterval = 100000;
+        int arraySize = Math.abs(beginningOfTheInterval) + endOfTheInterval + 1;
         int[] result = new int[arraySize];
         for (int i = 0; i < result.length; i++) {
             result[i] = i - 100000;
         }
         return result;
     }
+
     private static void divisionIntoPositiveAndNegativeNumbers(int[] result) {
         int dimensionArrayPositiveNumbers = 0;
         int dimensionArrayNegativeNumbers = 0;
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] < 0) {
+        for (int i : result) {
+            if (i < 0) {
                 dimensionArrayNegativeNumbers = dimensionArrayNegativeNumbers + 1;
             } else {
                 dimensionArrayPositiveNumbers = dimensionArrayPositiveNumbers + 1;
@@ -31,12 +31,12 @@ public class Main {
         int[] ArrayNegativeNumbers = new int[dimensionArrayNegativeNumbers];
         int positive = 0;
         int negative = 0;
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] < 0) {
-                ArrayNegativeNumbers[negative] = result[i];
+        for (int i : result) {
+            if (i < 0) {
+                ArrayNegativeNumbers[negative] = i;
                 negative = negative + 1;
             } else {
-                ArrayPositiveNumbers[positive] = result[i];
+                ArrayPositiveNumbers[positive] = i;
                 positive = positive + 1;
             }
         }
@@ -47,46 +47,42 @@ public class Main {
             System.out.print(element + " ");
         }*/
     }
+
+    private static boolean findPrimeNumbers(int number) {
+        boolean prime = false;
+        if (number >= 2) {
+            int temporary;
+            prime = true;
+            for (int j = 2; j <= number / 2; j++) {
+                temporary = number % j;
+                if (temporary == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+        return prime;
+    }
+
     private static int[] arrayPrimeNumbers(int[] result) {
         int dimensionArrayPrimeNumbers = 0;
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] >= 2) {
-                int temporary = 0;
-                boolean prime = true;
-                for (int j = 2; j <= result[i] / 2; j++) {
-                    temporary = result[i] % j;
-                    if (temporary == 0) {
-                        prime = false;
-                        break;
-                    }
-                }
-                if (prime) {
-                    dimensionArrayPrimeNumbers = dimensionArrayPrimeNumbers + 1;
-                }
+        for (int element : result) {
+            if (findPrimeNumbers(element)) {
+                dimensionArrayPrimeNumbers = dimensionArrayPrimeNumbers + 1;
             }
         }
-        int[] arrayPrimeNumbers = new int[dimensionArrayPrimeNumbers];
-        int k = 0;
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] >= 2) {
-                int temporary = 0;
-                boolean prime = true;
-                for (int j = 2; j <= result[i] / 2; j++) {
-                    temporary = result[i] % j;
-                    if (temporary == 0) {
-                        prime = false;
-                        break;
-                    }
-                }
-                if (prime) {
-                    arrayPrimeNumbers[k] = result[i];
-                    k = k + 1;
-                }
+        int[] arrayPrime = new int[dimensionArrayPrimeNumbers];
+        int timeDimension = 0;
+        for (int element : result) {
+            if (findPrimeNumbers(element)) {
+                arrayPrime[timeDimension] = element;
+                timeDimension = timeDimension + 1;
             }
         }
-        /*for (int element : arrayPrimeNumbers) {
-            System.out.print(element + " ");}*/
-        return arrayPrimeNumbers;
+        /*for (int i : arrayPrime) {
+            System.out.print(i + "  ");
+        }*/
+        return arrayPrime;
     }
 }
 
