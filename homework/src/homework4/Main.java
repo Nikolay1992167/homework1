232ec21c4674e89e22685069efd634bcc387a056
Nikolay1992167 <wulik1081@mail.ru>
2022-09-20 18:45:15 +0300

    decision

--- homework/src/homework4/Main.java
+++ homework/src/homework4/Main.java
@@ -3,12 +3,12 @@ package homework4;
 public class Main {
 
     public static void main(String[] args) {
-        String stringBeingСhecked = " Listen to the news from today and read the text at the same time. Listen to the news from today without reading the text. ";
+        String stringBeingСhecked = " Listen to the news from today and read the text at the   same time. Listen to the news from today without reading the text. ";
         calculationUniqueNumberWords(stringBeingСhecked);
     }
 
     private static int calculationUniqueNumberWords(String text) {
-        String[] arrayWords = text.trim().split("[^a-zA-Z0-9]");
+        String[] arrayWords = text.trim().replaceAll("[^a-zA-Z0-9 ]", "").split(" +");
         int calculation = 0;
         for (int i = 0; i < arrayWords.length; i++) {
             boolean uniqueness = true;
