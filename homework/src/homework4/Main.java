afd630833f5fffb8507cfd652677399e1b90d0c2
Nikolay1992167 <wulik1081@mail.ru>
2022-09-20 15:22:00 +0300

    decision

--- homework/src/homework4/Main.java
+++ homework/src/homework4/Main.java
@@ -4,23 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
         String stringBeingСhecked = " Listen to the news from today and read the text at the same time. Listen to the news from today without reading the text. ";
-        uniqueNumberWords(stringBeingСhecked);
-
-
+        calculationUniqueNumberWords(stringBeingСhecked);
     }
 
-    private static int uniqueNumberWords(String text) {
-        String[] arrayWords = text.trim().split("[\\s+.+]");
-        int totalNumberOfWords = arrayWords.length;
-        int availability = 0;
+    private static int calculationUniqueNumberWords(String text) {
+        String[] arrayWords = text.trim().split("[^a-zA-Z0-9]");
+        int calculation = 0;
         for (int i = 0; i < arrayWords.length; i++) {
-            for (int j = 0; j < arrayWords.length; j++) {
-                if (arrayWords[i] == arrayWords[j]) {
-                    availability = availability + 1;
+            boolean uniqueness = true;
+            for (int j = i + 1; j < arrayWords.length; j++) {
+                if (arrayWords[i].equalsIgnoreCase(arrayWords[j])) {
+                    uniqueness = false;
+                    break;
                 }
-
+            }
+            if (uniqueness) {
+                calculation = calculation + 1;
             }
         }
-        return 0;
+        System.out.println(calculation);
+        return calculation;
     }
 }
