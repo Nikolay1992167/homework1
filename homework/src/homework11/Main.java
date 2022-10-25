c2ecfd386f54f2821aa6671a39950dafa4598330
Nikolay1992167 <wulik1081@mail.ru>
2022-10-25 08:55:53 +0300

    homework11

---
+++ homework/src/homework11/Main.java
@@ -0,0 +1,26 @@
+package homework11;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> myList = Arrays.asList("a1", "a2", "a3", "b1", "b3", "c2", "c1", "c5");
+
+        List<String> result = myList.stream()
+                .filter((s) -> !s.contains("3"))
+                .sorted(Comparator.comparing(s -> s.replaceAll("\\D", "")))
+                .sorted(Comparator.comparing(s -> s.replaceAll("\\d", ""))).sorted(Comparator.reverseOrder())
+                .skip(1)
+                .map((s) -> s.replaceAll("1", "9"))
+                .map((s) -> s.toUpperCase())
+                .collect(Collectors.toList());
+        System.out.println(result.stream().collect(Collectors.joining(";")));
+
+    }
+
+}
